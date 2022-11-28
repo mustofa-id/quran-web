2cca754ff4425e9b8d956748659c364c9c3d6a87
mustofa-id <hm.dev7@gmail.com>
2022-11-28 20:47:17 +0700

    fix: build fail when prerendering

--- src/data/api.js
+++ src/data/api.js
@@ -22,7 +22,7 @@ async function api(path) {
 
 /** @returns {Promise<Surah[]>} */
 export function find_all_surah() {
-	return api('surat')
+	return api('surat').then((s) => s.map(fix_description))
 }
 
 /**
@@ -34,5 +34,19 @@ export function find_surah(number) {
 	if (number < 1 || number > 114) {
 		throw error(400, `Nomor surat tidak valid. Al-Quran terdiri dari 144 surat.`)
 	}
-	return api(`surat/${number}`)
+	return api(`surat/${number}`).then(fix_description)
+}
+
+/**
+ * Workaround for build error when prerendering pages
+ * caused by `deskripsi` from API that html formatted
+ * text and contains non-external link but not written
+ * as external link
+ *
+ * @param {Surah} surah
+ * @returns {any}
+ */
+function fix_description(surah) {
+	const deskripsi = surah.deskripsi.replace(`<a href="s002a001.htm">[10)</a>`, '10')
+	return { ...surah, deskripsi }
 }
