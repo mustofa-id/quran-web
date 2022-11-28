42f6b49b64836b74deb346c764e63aa4fafa3b13
mustofa-id <hm.dev7@gmail.com>
2022-11-28 23:08:02 +0700

    refactor: using client side filtering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,54 @@
 <script>
-	import { goto } from '$app/navigation'
 	import Seo from '$src/ui/seo.svelte'
 	import SurahItem from '$src/ui/surah-item.svelte'
 
 	/** @type {import('./$types').PageData} */
 	export let data
+
+	/** @type {string} */
+	let search_query
+
+	/** @type {Surah[]} */
+	$: all_surah = search_query
+		? data.all_surah.filter(filter_predicate(search_query))
+		: data.all_surah
+
+	/**
+	 * Simple surah filter predicate
+	 *
+	 * @param {string} search
+	 * @returns {(surah: Surah) => boolean}
+	 */
+	function filter_predicate(search) {
+		return (surah) => {
+			return (
+				surah.nomor === +search ||
+				surah.nama_latin.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+			)
+		}
+	}
 </script>
 
 <Seo />
 
 <article class="sm:max-w-3xl xl:max-w-6xl mx-auto my-4">
 	<section class="sticky top-2 z-1 m-2">
-		<form
-			on:submit|preventDefault={async (e) => {
-				// TODO: remove this submit handler when https://github.com/sveltejs/kit/issues/7251 got fixed
-
-				/** @type {any} */
-				const data = new FormData(e.currentTarget)
-				const params = new URLSearchParams(data)
-				await goto('?' + params, { keepFocus: true })
-			}}
+		<label
+			class="flex items-center space-x-2 border bg-light-200 
+			dark:bg-dark-800 focus-within:(outline outline-1) p-2"
 		>
-			<label
-				class="flex items-center space-x-2 border p-2 bg-light-200
-				 dark:bg-dark-800 focus-within:(outline outline-1)"
-			>
-				<div class="icon-search" />
-				<input
-					class="outline-none bg-transparent w-full"
-					type="search"
-					name="q"
-					value={data.search_query}
-					placeholder="Cari nama atau nomor surat"
-					on:input={async (e) => {
-						// refresh data when no search query
-						const { value } = e.currentTarget
-						if (!value) await goto('/', { keepFocus: true })
-					}}
-				/>
-			</label>
-		</form>
+			<div class="icon-search" />
+			<input
+				class="outline-none bg-transparent w-full"
+				placeholder="Cari nama atau nomor surat"
+				bind:value={search_query}
+				type="search"
+			/>
+		</label>
 	</section>
 
 	<section class="grid gap-2 sm:grid-cols-2 xl:grid-cols-3 mx-2">
-		{#each data.all_surah as surah (surah)}
+		{#each all_surah as surah (surah)}
 			<SurahItem {surah} />
 		{:else}
 			<article class="text-center col-span-3 m-2 p-4">
